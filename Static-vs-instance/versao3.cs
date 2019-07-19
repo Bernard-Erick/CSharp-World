e2fa895d696245112ce38dd57983516370e228ec
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-07-19 11:16:19 -0300

    Update versao3.cs

--- Static-vs-instance/versao3.cs
+++ Static-vs-instance/versao3.cs
@@ -2,7 +2,12 @@
 using System.Globalization;
 namespace CursoProg
 {
-    //Versão 2: Classe calculadora com método estático
+    /*Versão 2: Classe calculadora com método estático 
+    *Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+    *quanto seria o valor de uma circunferência e do volume de uma esfera
+    *para um raio daquele valor. Informar também o valor de PI com duas
+    *casas decimais.
+    */
 
     class Program
     {
