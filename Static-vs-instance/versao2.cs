662ac13e0af2582bb14c473a8d8908680cee311f
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-07-19 11:15:16 -0300

    Update versao2.cs

--- Static-vs-instance/versao2.cs
+++ Static-vs-instance/versao2.cs
@@ -2,7 +2,12 @@
 using System.Globalization;
 namespace CursoProg
 {
-    //Versão 2: Classe calculadora com membos de instância
+    /*Versão 2: Classe calculadora com membros de instância. 
+    *Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+    *quanto seria o valor de uma circunferência e do volume de uma esfera
+    *para um raio daquele valor. Informar também o valor de PI com duas
+    *casas decimais.
+    */
 
     class Program
     {
