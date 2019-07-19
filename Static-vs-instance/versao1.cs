b71a43c7d47fd66a825f55190734789726019241
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-07-19 11:13:31 -0300

    Update versao1.cs

--- Static-vs-instance/versao1.cs
+++ Static-vs-instance/versao1.cs
@@ -3,7 +3,11 @@ using System.Globalization;
 namespace CursoProg
 {
     /*Versão 1: métodos na própria classe do programa
-     * Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe
+     *          Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe
+     *Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+     *quanto seria o valor de uma circunferência e do volume de uma esfera
+     *para um raio daquele valor. Informar também o valor de PI com duas
+     *casas decimais.
      */
     class Program 
     {
