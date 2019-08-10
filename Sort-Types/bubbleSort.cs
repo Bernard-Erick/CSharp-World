1db4a2904e4ab343b7d5de9b2fd13629121252a6
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-08-10 10:04:56 -0300

    Add files via upload

---
+++ Sort-Types/bubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace CursoProg
+{
+    class Program
+    {
+        static void Main(string[] args) //Main
+        {
+            int[] numbers = { 60, 35, 18, 8, 14, 41, 3, 60, 80, 90, 20, 51, 60, 30, 5, 63, 2, 30, 5 };
+            bubbleSort(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(numbers[i]);
+
+        }
+        public static int[] bubbleSort(int[] vetor)
+        {
+            int aux;
+            for (int i = 0; i < vetor.Length-1; i++)
+            {
+
+                for (int j = 0; j < vetor.Length-1-i; j++)
+                {
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                    }
+                }
+            }
+            return vetor;
+        }
+
+
+    }
+
+
+}
