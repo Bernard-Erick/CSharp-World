e7f01917322cc2351ec2847ce0c14b0e0d15eb36
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-08-10 10:24:23 -0300

    Add files via upload

---
+++ Sort-Types/selectionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+namespace CursoProg
+{
+    class Program
+    {
+        static void Main(string[] args) //Main
+        {
+            int[] vetor = { 20, 35, 18, 8, 14, 41, 3, 39 };
+            selectionSort(vetor);
+            for (int i = 0; i < vetor.Length - 1; i++)
+                Console.Write(vetor[i]+", ");
+        }
+        static int[] selectionSort(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                int menor = vetor[i];
+                int posicaoMenor = i;
+                for (int z = i + 1; z < vetor.Length; z++)
+                {
+                    if (vetor[z] < menor)
+                    {
+                        menor = vetor[z];
+                        posicaoMenor = z;
+                    }
+
+                }
+                vetor[posicaoMenor] = vetor[i];
+                vetor[i] = menor;
+            }
+            
+            return vetor;
+        }
+
+
+
+    }
+
+
+}
