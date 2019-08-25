591ffcd3cce60418ca30c32136b1616eed728f40
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-08-25 12:29:10 -0300

    Add files via upload

---
+++ Conjuntos/Conjunto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace CursoProg
+{
+    class Program 
+    {
+        static void Main(string[] args) //Main
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+            Console.Write("O curso A possui quantos alunos?");
+            int n = int.Parse(Console.ReadLine());
+            int aluno;
+            Console.WriteLine("Digite o código dos alunos A:");
+            for (int i = 0; i < n; i++)
+            {
+                aluno = int.Parse(Console.ReadLine());
+                A.Add(aluno);
+            }
+            Console.Write("O curso B possui quantos alunos?");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o código dos alunos B:");
+            for (int i = 0; i < n; i++)
+            {
+                aluno = int.Parse(Console.ReadLine());
+                B.Add(aluno);
+            }
+            Console.Write("O curso C possui quantos alunos?");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o código dos alunos C:");
+            for (int i = 0; i < n; i++)
+            {
+                aluno = int.Parse(Console.ReadLine());
+                C.Add(aluno);
+            }
+            HashSet<int> Global = new HashSet<int>();
+            Global.UnionWith(A);
+            Global.UnionWith(B);
+            Global.UnionWith(C);
+            Console.WriteLine("Total de Alunos: " + Global.Count);
+            Console.WriteLine();
+        }      
+
+    }
+}
+
+
