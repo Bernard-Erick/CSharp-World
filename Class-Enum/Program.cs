cbd23540d50072fa59e42479a2f2a5bf5b0075cf
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-10-06 20:28:30 -0300

    Add files via upload

---
+++ Class-Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using CursoProg.Entities;
+using CursoProg.Entities.Enums;
+
+namespace CursoProg
+{
+    class Program 
+    {
+        static void Main(string[] args) //Main
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }      
+
+    }
+
+}
+
+
