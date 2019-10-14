e5c67e34e6829612f1e941a80a9f8bdc5c2e36fc
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-10-13 23:15:01 -0300

    Add files via upload

---
+++ Enum-Composition/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CursoProg.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+            //padrão
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ Enum-Composition/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoProg.Entities
+{
+    class HourContract
+    {
+        public DateTime Data { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+        public HourContract()
+        {
+            //padrão
+        }
+        public HourContract(DateTime data, double valuePerHour, int hours)
+        {
+            Data = data;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
