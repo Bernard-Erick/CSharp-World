08d2ebb82733862e243b29ce238c8ba5d7aa0bde
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-11-16 15:54:51 -0300

    Add files via upload

---
+++ Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoProg.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        public double Debt { get; set; }
+        public BusinessAccount() 
+        {
+            Balance = 10;
+        }
+        public BusinessAccount(int number, string holder, double balance,double loanLimit) : base(number,holder,balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount) 
+        {
+            if (amount <= LoanLimit) 
+            {
+                Balance += amount;
+                Debt += amount;
+            }
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CursoProg.Entities;
+namespace CursoProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010,"Erick Bernardo",100.0,500.0);
+            Console.WriteLine(account.Balance);
+            //account.Balance = 20;
+        }
+    }
+}
