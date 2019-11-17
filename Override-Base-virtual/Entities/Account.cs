132bb5df5fea0fd501a22ff33351f73967245495
Bernard Erick <49220123+Bernard-Erick@users.noreply.github.com>
2019-11-17 10:07:15 -0300

    Add files via upload

---
+++ Override-Base-virtual/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CursoProg.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() 
+        { 
+        }
+        public Account(int number,string holder,double balance) 
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public virtual void Withdraw(double amount) //Realizar Saque
+        {
+            Balance -= amount + 5.0;
+        }
+        public void Deposit(double amount) //Realizaar Deposito
+        {
+            Balance += amount;
+        }
+    }
+}
